deddf5f73500c91be5b9d25e1ed37b0434d42c9a
En1ey <llamosjohnhenley@gmail.com>
2025-08-31 14:26:16 +0800

    Initial commit from Create Next App

--- src/app/api/rsvp/route.ts
+++ src/app/api/rsvp/route.ts
@@ -3,7 +3,7 @@ import clientPromise from "@/app/lib/mongodb";
 
 export async function POST(request: Request) {
   const body = await request.json();
-  const client = await clientPromise;
+  const client = await clientPromise; 
   const db = client.db("wedding");
   const collection = db.collection("guests");
 
